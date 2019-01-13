dbdcdff3130ef5f92b8021bef4bae0e75c245855
Timo Appenzeller <timo@timoappenzeller.de>
2019-01-13 16:42:10 +0100

    Make application startable with arguments

--- src/main/java/de/hs_heilbronn/seb/projektstudien/lyricsanalyse/Application.java
+++ src/main/java/de/hs_heilbronn/seb/projektstudien/lyricsanalyse/Application.java
@@ -2,6 +2,7 @@ package de.hs_heilbronn.seb.projektstudien.lyricsanalyse;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -21,9 +22,35 @@ public class Application {
 
 		logger.info("Application started...");
 
+		int fromYear = 2018;
+		int toYear = 2018;
+
+		if (args.length > 0) {
+			if (args.length == 2) {
+				try {
+					fromYear = Integer.parseInt(args[0]);
+					toYear = Integer.parseInt(args[1]);
+				} catch (NumberFormatException e) {
+					logger.error("Arguments cannot be parsed to integer.");
+					throw new IllegalArgumentException("Invalid arguments. Please try again.");
+				}
+
+				if (toYear < fromYear) {
+					logger.error("Year to start crawling has to be less than year to end crawling.");
+					throw new IllegalArgumentException("Invalid arguments. Please try again.");
+				}
+			}
+			if (args.length == 1) {
+				fromYear = Integer.parseInt(args[0]);
+				toYear = fromYear;
+			}
+		}
+
 		LyricsCrawler lyricsCrawler = new LyricsCrawler();
 
-		List<Song> allSongs = ChartCrawler.crawl(2017, 2017);
+		logger.info("Start crawling from year {} to year {}", fromYear, toYear);
+
+		List<Song> allSongs = ChartCrawler.crawl(fromYear, toYear);
 
 		logger.info("Crawled {} songs", allSongs.size());
 
